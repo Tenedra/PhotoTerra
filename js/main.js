f43be5fd0be6e7e638d5798b549291e893c6c927
Maria <masha8933m@gmail.com>
2021-04-13 12:58:50 +0300

    comment

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){ 
+    $('.slider').slick({
+        slidesToShow: 1,
+        dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 2000,
+        fade: true,
+        pauseOnHover: false
+ });
+ });
+
+ $(function(){ 
+    $('.collection').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        speed: 2000,
+        pauseOnHover: false
+ });
+ });
